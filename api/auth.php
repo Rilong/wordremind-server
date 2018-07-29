68505d77113b81ff596c102823dffbcc505ddafe
Rilong <romgnatyuk@gmail.com>
2018-07-29 14:19:59 +0300

    Password

--- api/auth.php
+++ api/auth.php
@@ -17,7 +17,7 @@ $router->get('/api/auth', function (Request $request, Response $response) {
 });
 
 $router->post('/api/auth', function (Request $request, Response $response) {
-    $login = htmlentities($request->login);
+    $login = htmlspecialchars($request->login);
     $password = htmlspecialchars($request->password);
 
     $user = R::findOne('users', 'login = ?', array($login));
@@ -33,10 +33,24 @@ $router->post('/api/auth', function (Request $request, Response $response) {
 
     $response->code(200);
     $token = Jwt::generateToken(array(
-        'user' => $user->export()
+        'user' => array('id' => $user->id, 'login' => $user->login)
     ), Date::hour(1));
 
+    $user->auth_token = $token;
+    R::store($user);
+
     return Json::encode(array(
-        'access_token' => $token
+        'user' => $user->export()
     ));
+});
+
+$router->post('/api/logout', function (Request $request, Response $response) {
+    $login = $request->login;
+    $password = $request->password;
+
+    $user = Passport::signByHash($login, $password);
+    if ($user) {
+        $user->auth_token = null;
+        R::store($user);
+    }
 });
--- helpers/Passport.php
+++ helpers/Passport.php
@@ -2,6 +2,8 @@
 
 namespace helpers;
 
+use RedBeanPHP\R;
+
 abstract class Passport {
 
     public static function hash($password) {
@@ -10,5 +12,13 @@ abstract class Passport {
 
     public static function verify($password, $hash_password) {
         return password_verify($password, $hash_password);
+    }
+
+    public static function signByHash($login, $password) {
+        $user = R::findOne('users','`login` = ?', array($login));
+        if ($user->count() != 0 && $password == $user->password) {
+            return $user;
+        }
+        return false;
     }
 }
