e6e22e5b0c10cc77b90abc3e3dab308287392b86
Rilong <romgnatyuk@gmail.com>
2018-09-07 11:40:31 +0300

    Changed sort

--- functions.php
+++ functions.php
@@ -90,13 +90,6 @@ function getWordsAndSentencesTree($data) {
 function wordsSQL($settings = null) {
     $sql = "SELECT `words`.`id`, `words`.`user_id`, `words`.`word`, `words`.`created_date`,  `words`.`translation` AS `word_translation`,`sentences`.`id` AS `sentence_id`, `sentences`.`text` AS `sentence_text`, `sentences`.`text` AS `sentence_text`, `sentences`.`translation` AS `sentence_translation` FROM `words` 
     LEFT JOIN `sentences` ON `words`.`id` = `sentences`.`word_id` 
-    WHERE `user_id` = ? ORDER BY `words`.`id`";
-
-    if ($settings['onlyNew'] == 'true') {
-        $sql = "SELECT `words`.`id`, `words`.`user_id`, `words`.`word`, `words`.`created_date`, `words`.`translation`  AS `word_translation`,`sentences`.`id` AS `sentence_id`, `sentences`.`text` AS `sentence_text`, `sentences`.`text` AS `sentence_text`, `sentences`.`translation` AS `sentence_translation` FROM `words` 
-    LEFT JOIN `sentences` ON `words`.`id` = `sentences`.`word_id` 
-    WHERE `user_id` = ? ORDER BY `words`.`created_date` DESC LIMIT 8";
-    }
-
+    WHERE `user_id` = ? ORDER BY `words`.`id` DESC";
     return $sql;
 }
